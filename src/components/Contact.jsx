8fc8afbf11beb0831c3391ad99518ec60e197656
James-Christensen <109244105+James-Christensen@users.noreply.github.com>
2023-02-15 09:00:51 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { FaUser } from "react-icons/fa";
 
 export default function Contact({title, phone, email, name, icon}) {
   return (
-    <div className="card card-compact w-64 bg-base-100 shadow-xl">
+    <div className="card card-compact w-full bg-base-100 shadow-xl">
       <figure>
         {icon}
       </figure>
